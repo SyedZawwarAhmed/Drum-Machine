28a6221f5f4ab02828bbf5aa8607070e020cf9e0
SyedZawwarAhmed <64926760+SyedZawwarAhmed@users.noreply.github.com>
2020-11-14 15:39:36 +0500

    Update App.js

--- App.js
+++ App.js
@@ -8,13 +8,11 @@ const audio7 = document.getElementById("Z");
 const audio8 = document.getElementById("X");
 const audio9 = document.getElementById("C");
 const note = document.getElementById("note");
-const drumPad = document.getElementsByClassName("drum-pad");
 
 function bell1() {
     audio1.currentTime = 0;
     audio1.play();
     note.innerText = "c2";
-    
 }
 
 function bell2() {
@@ -59,66 +57,69 @@ function bell9() {
 }
 
 window.addEventListener("keydown", function(e) {
+
+    const style = "{height: 95px; width: 95px; margin: 12.5px;}";
+
     if (e.key === 'Q' || e.key === 'q') {
         bell1();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#a {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#a" + style);
         x.appendChild(t);
         document.head.appendChild(x);     
     }
     if (e.key === 'W' || e.key === 'w') {
         bell2();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#b {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#b" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
     if (e.key === 'E' || e.key === 'e') {
         bell3();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#c {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#c" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
     if (e.key === 'A' || e.key === 'a') {
         bell4();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#d {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#d" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
     if (e.key === 'S' || e.key === 's') {
         bell5();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#e {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#e" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
     if (e.key === 'D' || e.key === 'd') {
         bell6();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#f {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#f" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
     if (e.key === 'Z' || e.key === 'z') {
         bell7();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#g {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#g" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
     if (e.key === 'X' || e.key === 'x') {
         bell8();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#h {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#h" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
     if (e.key === 'C' || e.key === 'c') {
         bell9();
         const x = document.createElement("STYLE");
-        const t = document.createTextNode("#i {height: 95px; width: 95px; margin: 12.5px;}");
+        const t = document.createTextNode("#i" + style);
         x.appendChild(t);
         document.head.appendChild(x);
     }
